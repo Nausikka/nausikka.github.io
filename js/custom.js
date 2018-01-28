277c111db1f5ae23c9eafaa888fe463aa8cd4438
Jana Grajciarova <jana@repositive.io>
2018-01-28 22:53:42 +0000

    tweaking card, hover on navbar

--- js/custom.js
+++ js/custom.js
@@ -2,23 +2,6 @@ $(document).ready(function() {
 $(function()) {
     $('#nav').affix({
     offset: { top: $('#nav').offset().top }
-});
-    
-} 
-​
-
-		
-	// $(function() {
- //    $('#nav-wrapper').height($("#nav").height());
-    
- //    $('#nav').affix({
- //        offset: { top: $('#nav').offset().top }
- //    	});
-	// });
-
-	// window.scrollTo = function( x,y ) 
- //    {
- //        return true;
- //    }
-
+		});
+    } 
 });
